44428686ef44622281c6269b1e5275b581a85ed1
Paul Triana <paul_triana@sfu.ca>
2026-07-06 04:55:44 +0200

    fix: guard training package initialization from missing lightning package

--- src/python/midigpt/training/__init__.py
+++ src/python/midigpt/training/__init__.py
@@ -1,16 +1,27 @@
 from midigpt.training.collator import MidiGPTCollator
-from midigpt.training.data_module import MidiGPTDataModule
 from midigpt.training.dataset import _SCORE_SCHEMA, DatasetBuilder, MidiGPTDataset
-from midigpt.training.lightning_module import MidiGPTLightningModule
 from midigpt.training.trainer import TrainConfig, train
+
+try:
+    from midigpt.training.data_module import MidiGPTDataModule
+    from midigpt.training.lightning_module import MidiGPTLightningModule
+    _has_lightning = True
+except ImportError:
+    MidiGPTDataModule = None  # type: ignore[assignment,misc]
+    MidiGPTLightningModule = None  # type: ignore[assignment,misc]
+    _has_lightning = False
 
 __all__ = [
     "_SCORE_SCHEMA",
     "DatasetBuilder",
     "MidiGPTCollator",
-    "MidiGPTDataModule",
     "MidiGPTDataset",
-    "MidiGPTLightningModule",
     "TrainConfig",
     "train",
 ]
+
+if _has_lightning:
+    __all__.extend([
+        "MidiGPTDataModule",
+        "MidiGPTLightningModule",
+    ])
